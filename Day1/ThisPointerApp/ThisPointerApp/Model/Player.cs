4cbbe115b47726b645336f5f1c2a2bcae07f5d8d
kannan <kannan.mumbai@yahoo.com>
2023-10-26 13:18:43 +0530

    'thispointer'

---
+++ Day1/ThisPointerApp/ThisPointerApp/Model/Player.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThisPointerApp.Model
+{
+    internal class Player
+    {
+        private int _age;
+        private string _name;
+        private static int _counter;
+
+
+        static Player() {
+            //_counter = 100;
+            Console.WriteLine("Inside static constructor");
+
+        }
+        public Player(int _age, string _name) {
+            this._age = _age;
+            this._name = _name;
+            Console.WriteLine("constructor with 2 params");
+            _counter += 1;
+        }
+
+        public Player(string x):this(18,x) {
+            Console.WriteLine("constructor with 1 params");
+        }
+        public int Age {
+            get {  return _age; }
+        }
+
+        public string Name { get { return _name; } }
+
+        public Player WhoIsElder(Player other)
+        {
+           
+            return this._age > other.Age ? this : other;
+        }
+
+        //public int Counter { 
+        
+        //get { return _counter; } 
+        
+        //}
+
+        public static int HeadCount {
+            get { 
+             return _counter;
+            }
+        }
+    }
+}
---
+++ Day1/ThisPointerApp/ThisPointerApp/Program.cs
@@ -0,0 +1,53 @@
+﻿using ThisPointerApp.Model;
+
+namespace ThisPointerApp
+{
+    internal class Program
+    {
+        
+
+        
+        static void Main(string[] args)
+        {
+            // CaseStudy1();
+
+            // PrintDetails(new Player("kannan"));//Anonymous object
+
+
+            var p1 = new Player("A");
+            //Console.WriteLine(p1.Counter);
+            Console.WriteLine(Player.HeadCount);
+
+            var p2 = new Player("B");
+            //  Console.WriteLine(p1.Counter);
+            //  Console.WriteLine(Player.GetHeadCount());
+            Console.WriteLine(Player.HeadCount);
+
+            var p3 = new Player("C");
+            Console.WriteLine(Player.HeadCount);
+
+            //  Console.WriteLine(p1.Counter);
+            // Console.WriteLine(Player.GetHeadCount());
+        }
+
+        private static void PrintDetails(Player player)
+        {
+            Console.WriteLine("name is "+player.Name);
+            Console.WriteLine("age is " + player.Age);
+        }
+
+        private static void CaseStudy1()
+        {
+            var p1 = new Player(20, "Sachin");
+
+            var p2 = new Player(30, "Vicky");
+
+            var elderPlayer = p1.WhoIsElder(p2);
+
+            Console.WriteLine("name and age of elder player");
+
+
+            Console.WriteLine($"name is {p1.Name} age is {p1.Age}");
+        }
+    }
+}
